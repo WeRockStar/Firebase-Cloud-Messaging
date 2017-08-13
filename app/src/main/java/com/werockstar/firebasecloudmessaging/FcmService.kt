9a2aefed97aa9e5ecdc0c54dbe1f7fc8785e408e
WeRockStar <kotchaphan.m@gmail.com>
2017-08-13 21:38:57 +0700

    Add receive message from FCM

--- app/src/main/java/com/werockstar/firebasecloudmessaging/FcmService.kt
+++ app/src/main/java/com/werockstar/firebasecloudmessaging/FcmService.kt
@@ -5,6 +5,7 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.media.RingtoneManager
 import android.support.v7.app.NotificationCompat
 import android.util.Log
@@ -16,22 +17,24 @@ class FcmService : FirebaseMessagingService() {
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
 
         Log.d(TAG, "From: " + remoteMessage.from)
-        Log.d(TAG, "Notification Message Body: " + remoteMessage.notification.getBody())
-        sendNotification(remoteMessage.notification.getBody())
+        Log.d(TAG, "Notification Message Body: " + remoteMessage.notification.body)
+        sendNotification(remoteMessage.notification)
     }
 
-    private fun sendNotification(messageBody: String?) {
+    fun sendNotification(notification: RemoteMessage.Notification) {
         val intent = Intent(this, MainActivity::class.java)
+        intent.putExtra(MESSAGE, notification.body)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-        val pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
-                PendingIntent.FLAG_ONE_SHOT)
+
+        val pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent, PendingIntent.FLAG_ONE_SHOT)
 
         val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
         val notificationBuilder = NotificationCompat.Builder(this)
                 .setSmallIcon(R.drawable.ic_launcher)
-                .setContentTitle("Title")
-                .setContentText(messageBody)
+                .setContentTitle(notification.title)
+                .setContentText(notification.body)
                 .setAutoCancel(true)
+                .setColor(Color.parseColor("#4B8A08"))
                 .setSound(defaultSoundUri)
                 .setContentIntent(pendingIntent)
 
@@ -41,7 +44,7 @@ class FcmService : FirebaseMessagingService() {
     }
 
     companion object {
-
-        private val TAG = "MESSAGE"
+        val TAG = FcmService::class.java.simpleName
+        val MESSAGE = "MESSAGE"
     }
 }
--- app/src/main/java/com/werockstar/firebasecloudmessaging/MainActivity.kt
+++ app/src/main/java/com/werockstar/firebasecloudmessaging/MainActivity.kt
@@ -2,13 +2,20 @@ package com.werockstar.firebasecloudmessaging
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var tvMessage: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+
+        tvMessage = findViewById(R.id.tvMessage) as TextView
+        tvMessage.text = intent.getStringExtra("MESSAGE") ?: ""
+
     }
 
 }
