9af2b7b4e3dbaa19e12d6b9945f9bd6da814a380
WeRockStar <kotchaphan.m@gmail.com>
2017-08-15 23:37:23 +0700

    Add custom notification with image

--- app/src/main/java/com/werockstar/firebasecloudmessaging/FcmService.kt
+++ app/src/main/java/com/werockstar/firebasecloudmessaging/FcmService.kt
@@ -5,11 +5,13 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.media.RingtoneManager
-import android.support.v7.app.NotificationCompat
+import android.support.v4.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import java.net.URL
 
 class FcmService : FirebaseMessagingService() {
 
@@ -19,8 +21,12 @@ class FcmService : FirebaseMessagingService() {
     }
 
     private fun showNotification(remoteMessage: RemoteMessage) {
+        val notificationData = remoteMessage.data
+        val imageUrl = notificationData["image"]
+        val notification = remoteMessage.notification
+
         val intent = Intent(this, MainActivity::class.java)
-        intent.putExtra(MESSAGE, remoteMessage.notification.body)
+        intent.putExtra(MESSAGE, notification.body)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
 
         val pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent, PendingIntent.FLAG_ONE_SHOT)
@@ -28,15 +34,21 @@ class FcmService : FirebaseMessagingService() {
         val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
         val notificationBuilder = NotificationCompat.Builder(this)
                 .setSmallIcon(R.drawable.ic_launcher)
-                .setContentTitle(remoteMessage.notification.title)
-                .setContentText(remoteMessage.notification.body)
+                .setContentTitle(notification.title)
+                .setContentText(notification.body)
                 .setAutoCancel(true)
                 .setColor(Color.parseColor("#4B8A08"))
                 .setSound(defaultSoundUri)
                 .setContentIntent(pendingIntent)
 
-        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val url = URL(imageUrl)
+        val bitmap = BitmapFactory.decodeStream(url.openConnection().getInputStream())
+        /*
+            Support library v4 provide BigPictureStyle class
+         */
+        notificationBuilder.setStyle(NotificationCompat.BigPictureStyle().bigPicture(bitmap).setSummaryText(notification.body))
 
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
     }
 
