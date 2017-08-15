80f0418bab2a64cee9f31411f2f420ce97da2e3c
WeRockStar <kotchaphan.m@gmail.com>
2017-08-15 22:25:55 +0700

    Prepare subscribe topic

--- app/src/main/java/com/werockstar/firebasecloudmessaging/MainActivity.kt
+++ app/src/main/java/com/werockstar/firebasecloudmessaging/MainActivity.kt
@@ -2,11 +2,14 @@ package com.werockstar.firebasecloudmessaging
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var tvMessage: TextView
+    private lateinit var tvMessage: TextView
+    private lateinit var btnSubscribe: Button
+    private lateinit var btnUnsubscribe: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,7 +17,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         tvMessage = findViewById(R.id.tvMessage) as TextView
+        btnSubscribe = findViewById(R.id.btnSubscribe) as Button
+        btnUnsubscribe = findViewById(R.id.btnUnsubscribe) as Button
+
         tvMessage.text = intent.getStringExtra("MESSAGE") ?: ""
+
+    }
+
+    fun onUnsubscribe() {
+
+    }
+
+    fun onSubscribe() {
 
     }
 
